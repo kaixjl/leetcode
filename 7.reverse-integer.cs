e33274cdcb2bb535f554d49f1e049b36dd66636f
Xiu HX <kaixjl@163.com>
2020-11-17 20:19:38 +0800

    init

---
+++ 7.reverse-integer.cs
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=7 lang=csharp
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+using System;
+using System.Collections.Generic;
+public class Solution {
+
+    public int Reverse(int x) {
+        int rev = 0;
+        while (x!=0) 
+        {
+            int n = x%10;
+            x=x/10;
+            if(rev>214748364 || rev==214748364 && n > 7)
+            {
+                rev=0;
+                break;
+            }
+            else if(rev<-214748364 || rev==214748364 && n<-8)
+            {
+                rev=0;
+                break;
+            }
+            rev=rev*10+n;
+
+        }
+        return rev;
+    }
+
+    // public int Reverse(int x) {
+    //     List<int> numberList = new List<int>(10);
+    //     while (x!=0) 
+    //     {
+    //         numberList.Add(x%10);
+    //         x=x/10;
+    //     }
+    //     foreach(var n in numberList)
+    //     {
+    //         if(x>214748364 || x==214748364 && n > 7)
+    //         {
+    //             x=0;
+    //             break;
+    //         }
+    //         else if(x<-214748364 || x==214748364 && n<-8)
+    //         {
+    //             x=0;
+    //             break;
+    //         }
+    //         x=x*10+n;
+    //     }
+    //     return x;
+    // }
+}
+
+// @lc code=end
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Solution sln = new Solution();
+            Trace.Assert(sln.Reverse(1234)==4321);
+            Trace.Assert(sln.Reverse(-1234)==-4321);
+            Trace.Assert(sln.Reverse(120)==21);
+            Trace.Assert(sln.Reverse(0)==0);
+            Trace.Assert(sln.Reverse(1534236469)==0);
+            Trace.Assert(sln.Reverse(-1534236469)==0);
+        }
+    }
+}
