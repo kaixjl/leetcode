431dfeada7db4f07093630eb90cd8ef403af2814
Xiu HX <kaixjl@163.com>
2020-11-18 19:04:00 +0800

    P4v1

--- 4.median-of-two-sorted-arrays.cs
+++ 4.median-of-two-sorted-arrays.cs
@@ -8,24 +8,228 @@ using System.Collections.Generic;
 // @lc code=start
 public partial class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        var ln = Console.ReadLine();
-        int idx1l = ln.IndexOf('[') + 1;
-        int idx1r = ln.IndexOf(']', idx1l + 1);
-        int idx2l = ln.IndexOf('[', idx1r + 1) + 1;
-        int idx2r = ln.IndexOf(']', idx2l + 1);
-        string str1 = ln.Substring(idx1l, idx1r-idx1l);
-        string str2 = ln.Substring(idx2l, idx2r-idx2l);
-        int len1 = str1.Length;
-        int len2 = str2.Length;
-        int start = 0;
-        int next = 0;
-
-        next = str1.IndexOf(',', start);
-        while(next<len1)
+        int start1 = 0, start2 = 0, end1 = nums1.Length, end2 = nums2.Length;
+        int len_total = end1 + end2;
+        int len_half = len_total / 2;
+        double result;
+        if (nums1.Length==0) // spectial case
         {
+            if(nums2.Length % 2 ==1) // odd
+            {
+                result = nums2[nums2.Length/2];
+                return result;
+            }
+            else
+            {
+                result = Convert.ToDouble(nums2[nums2.Length/2] + nums2[nums2.Length/2-1]) / 2;
+                return result;
+            }
+        }
+        else if (nums2.Length==0) // spectial case
+        {
+            if(nums1.Length % 2 ==1) // odd
+            {
+                result = nums1[nums1.Length/2];
+                return result;
+            }
+            else
+            {
+                result = Convert.ToDouble(nums1[nums1.Length/2] + nums1[nums1.Length/2-1]) / 2;
+                return result;
+            }
+        }
+        
+        while (true)
+        {
+            if(Process(nums1, nums2, ref start1, ref end1, ref start2, ref end2, len_total, len_half, out result))
+                return result;
             
+            if(Process(nums2, nums1, ref start2, ref end2, ref start1, ref end1, len_total, len_half, out result))
+                return result;
         }
 
+    }
+
+    public bool Process(int[] nums1, int[] nums2, ref int start1, ref int end1, ref int start2, ref int end2, int len_total, int len_half, out double result)
+    {
+        int index1 = (start1 + end1) / 2;
+        int curr1 = nums1[index1];
+
+        if (start2==end2)
+        {
+            int len_of_nums1_left = start1;
+            int len_of_nums1_right = nums1.Length - end1;
+            int len_of_nums2_left = start2;
+            int len_of_nums2_right = nums2.Length - end2;
+
+            int len_of_left = len_of_nums1_left + len_of_nums2_left;
+            int len_of_right = len_of_nums1_right + len_of_nums2_right;
+
+            int diff = len_of_left - len_of_right;
+
+            if(diff>0)
+            {
+                end1 -= diff;
+            }
+            else
+            {
+                start1 -= diff;
+            }
+
+            int sum = end1 + start1;
+            index1 = sum/2;
+            if(sum%2==1)
+            {
+                result = Convert.ToDouble(nums1[index1]);
+                return true;
+            }
+            else
+            {
+                curr1 = nums1[index1];
+                int another=0;
+                if(index1 == 0) another = nums2[start2-1];
+                else if(start2 == 0) another = nums1[index1-1];
+                else another = Math.Max(nums1[index1-1], nums2[start2-1]);
+                
+                result = (Convert.ToDouble(another) + curr1) / 2;
+                return true;
+            }
+        }
+
+
+        int index1r = FindRight(nums1, start1, end1, index1);
+        index1 = FindLeftEquals(nums1, start1, end1, index1);
+        int idx = BinarySearch(nums2, start2, end2, curr1);
+
+        int idxr = 0;
+
+        if(idx==end2) // out of bound
+        {
+            idxr = idx;
+        }
+        else if(nums2[idx] == curr1)
+        {
+            idxr = FindRight(nums2, start2, end2, idx);
+        }
+        else
+        {
+            idxr = idx;
+        }
+
+        int len_of_curr1 = index1r - index1 + idxr - idx;
+        int len_of_curr1_l = index1 - 0 + idx - 0;
+        int len_of_curr1_r = nums1.Length - index1r + nums2.Length - idxr;
+
+        if (len_half < len_of_curr1_l)
+        {
+            end1 = index1;
+        }
+        else if(len_half < len_of_curr1_l + len_of_curr1) // median is in it
+        {
+            if (len_total%2==1) // odd
+            {
+                result = curr1;
+                return true;
+            }
+            else // even
+            {
+                if (len_half == len_of_curr1_l) // 边界条件
+                {
+                    int another=0;
+                    if(index1 == 0) another = nums2[idx-1];
+                    else if(idx == 0) another = nums1[index1-1];
+                    else another = Math.Max(nums1[index1-1], nums2[idx-1]);
+                    
+                    result = (Convert.ToDouble(another) + curr1) / 2;
+                    return true;
+                }
+                else
+                {
+                    result = curr1;
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            start1 = index1r;
+        }
+        
+        result = 0;
+        return false;
+
+    }
+
+    public int BinarySearch(int[] nums, int start, int end, int value)
+    {
+        int start0 = start, end0 = end;
+        if (start == end) return end;
+
+        int index = (start + end) / 2;
+        int curr = nums[index];
+        while(end - start > 1)
+        {
+            if (value < curr)
+            {
+                end = index;
+            } 
+            else
+            {
+                start = index;
+            }
+            index = (start + end) / 2;
+            curr = nums[index];
+        }
+
+        if(value > curr)
+        {
+            index = FindRight(nums, start0, end0, index);
+        }
+        else
+        {
+            index = FindLeftEquals(nums, start0, end0, index);
+        }
+
+        return index;
+    }
+
+    public int FindLeftEquals(int[] nums, int start, int end, int index)
+    {
+        int value = nums[index];
+        while(index >= start && nums[index] == value)
+        {
+            index--;
+        }
+        index++;
+        return index;
+    }
+    // public int FindLeft(int[] nums, int start, int end, int index)
+    // {
+    //     int value = nums[index];
+    //     while(index >= start && nums[index] == value)
+    //     {
+    //         index--;
+    //     }
+    //     return index;
+    // }
+    // public int FindRightEquals(int[] nums, int start, int end, int index)
+    // {
+    //     int value = nums[index];
+    //     while(index < end && nums[index] == value)
+    //     {
+    //         index++;
+    //     }
+    //     index--;
+    //     return index;
+    // }
+    public int FindRight(int[] nums, int start, int end, int index)
+    {
+        int value = nums[index];
+        while(index < end && nums[index] == value)
+        {
+            index++;
+        }
+        return index;
     }
 }
 // @lc code=end
--- Program.cs
+++ Program.cs
@@ -9,12 +9,50 @@ namespace leetcode
         {
             Console.WriteLine("Hello World!");
             Solution sln = new Solution();
-            Trace.Assert(sln.Reverse(1234)==4321);
-            Trace.Assert(sln.Reverse(-1234)==-4321);
-            Trace.Assert(sln.Reverse(120)==21);
-            Trace.Assert(sln.Reverse(0)==0);
-            Trace.Assert(sln.Reverse(1534236469)==0);
-            Trace.Assert(sln.Reverse(-1534236469)==0);
+            //P7(sln);
+            P4_BinarySearchTest(sln);
+            P4(sln);
+        }
+
+        static void AssertEquals<T>(IEquatable<T> val1, IEquatable<T> val2)
+        {
+            if(!val1.Equals(val2))
+            {
+                throw new Exception($"Equation Assertion Failed. val1 = {val1} but val2 = {val2}.");
+            }
+        }
+
+        static void P7(Solution sln)
+        {
+            AssertEquals(sln.Reverse(1234), 4321);
+            AssertEquals(sln.Reverse(-1234), -4321);
+            AssertEquals(sln.Reverse(120), 21);
+            AssertEquals(sln.Reverse(0), 0);
+            AssertEquals(sln.Reverse(1534236469), 0);
+            AssertEquals(sln.Reverse(-1534236469), 0);
+        }
+
+        static void P4(Solution sln)
+        {
+            AssertEquals(sln.FindMedianSortedArrays(new[]{1,3}, new[]{2}), 2.0);
+            AssertEquals(sln.FindMedianSortedArrays(new[]{1,2}, new[]{3,4}), 5.0 / 2);
+            AssertEquals(sln.FindMedianSortedArrays(new[]{0,0}, new[]{0,0}), 0.0);
+            AssertEquals(sln.FindMedianSortedArrays(new int[]{}, new[]{1}), 1.0);
+            AssertEquals(sln.FindMedianSortedArrays(new[]{2}, new int[]{}), 2.0);
+            AssertEquals(sln.FindMedianSortedArrays(new[]{2}, new int[]{1,3,4}), 2.5);
+        }
+
+        static void P4_BinarySearchTest(Solution sln)
+        {
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 0, 15, 0), 0);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 0, 15, 1), 2);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 0, 15, 2), 3);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 0, 15, 4), 5);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 0, 15, 10), 12);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 2, 12, 0), 2);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 2, 12, -1), 2);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 2, 12, 10), 12);
+            AssertEquals(sln.BinarySearch(new[]{0,0,1,2,3,4,4,5,6,7,8,9,10,10,10}, 2, 12, 15), 12);
         }
     }
 }
