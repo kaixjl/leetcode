588755e4998e5386dbb331c5dcdda31fee4f947e
Xiu HX <kaixjl@163.com>
2021-06-24 20:27:40 +0800

    P295v1.1 改一下PriorityQueue，没什么意义

--- 295.find-median-from-data-stream.cs
+++ 295.find-median-from-data-stream.cs
@@ -76,15 +76,16 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace LP295 {
 public class PriorityQueueHelper {
-    static void PrintPriorityQueue<T>(PriorityQueue<T> priorityQueue) where T: IComparable<T> {
+    static void PrintPriorityQueue<K, V>(PriorityQueue<K, V> priorityQueue) where K: IComparable<K> {
         StringBuilder sb = new StringBuilder();
         int ptr = 0;
         for(int p = 1; p < priorityQueue.Count; p *= 2) {
             for(int i = 0; i < p; i++) {
-                sb.AppendFormat("{0} ", priorityQueue.list[ptr++]);
+                sb.AppendFormat("{0} ", priorityQueue.list[ptr++].value);
                 if(ptr >= priorityQueue.Count) break;
             }
             sb.AppendLine();
@@ -92,82 +93,118 @@ public class PriorityQueueHelper {
         Console.Write(sb.ToString());
     }
 }
+
 // @lc code=start
-public class PriorityQueue<T> where T: IComparable<T> {
-    internal List<T> list;
+public class PriorityQueueEntity<TKey, TValue> {
+    public TKey key;
+    public TValue value;
+
+    public PriorityQueueEntity(TKey key, TValue value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+public class PriorityQueue<TKey, TValue> {
+    internal List<PriorityQueueEntity<TKey, TValue>> list;
     bool ascent = false;
-    public PriorityQueue(bool ascent = false) {
-        list = new List<T>();
+    private readonly IComparer<TKey> comparer;
+    public PriorityQueue(IComparer<TKey> comparer, bool ascent = false) {
+        list = new List<PriorityQueueEntity<TKey, TValue>>();
         this.ascent = ascent;
+        this.comparer = comparer;
     }
 
-    public PriorityQueue(int capacity, bool ascent = false) {
-        list = new List<T>(capacity);
+    public PriorityQueue(bool ascent = false): this(Comparer<TKey>.Default, ascent) { }
+
+    public PriorityQueue(int capacity, IComparer<TKey> comparer, bool ascent = false) {
+        list = new List<PriorityQueueEntity<TKey, TValue>>(capacity);
         this.ascent = ascent;
+        this.comparer = comparer;
     }
 
-    public PriorityQueue(IEnumerable<T> collection, bool ascent = false) {
-        list = new List<T>(collection);
+    public PriorityQueue(int capacity, bool ascent = false): this(capacity, Comparer<TKey>.Default, ascent) { }
+
+    public PriorityQueue(IEnumerable<TKey> keys, IEnumerable<TValue> values, IComparer<TKey> comparer, bool ascent = false) {
+        list = new List<PriorityQueueEntity<TKey, TValue>>();
+        foreach(var (key, value) in keys.Zip(values)) {
+            list.Add(new PriorityQueueEntity<TKey, TValue>(key, value));
+        }
         this.ascent = ascent;
-        ConstructPriorityQueueWithExistingList(list, this.ascent);
+        this.comparer = comparer;
+
+        ConstructPriorityQueueWithExistingList(list, this.comparer, this.ascent);
     }
 
-    static void ConstructPriorityQueueWithExistingList(List<T> list, bool ascent = false) {
+    public PriorityQueue(IEnumerable<TKey> keys, IEnumerable<TValue> values, bool ascent = false): this(keys, values, Comparer<TKey>.Default, ascent) { }
+
+    static void ConstructPriorityQueueWithExistingList(List<PriorityQueueEntity<TKey, TValue>> list, IComparer<TKey> comparer, bool ascent = false) {
         for(int i = list.Count / 2 - 1; i >= 0; i--)
-            FilterDown(list, i, ascent);
+            FilterDown(list, i, comparer, ascent);
     }
 
-    static void FilterDown(List<T> list, int index, bool ascent = false) {
+    static void FilterDown(List<PriorityQueueEntity<TKey, TValue>> list, int index, IComparer<TKey> comparer, bool ascent = false) {
         int idx_lc = index * 2 + 1;
         int idx_rc = index * 2 + 2;
         if (idx_lc >= list.Count) return;
         else if (idx_rc >= list.Count) {
-            if ((ascent ? -1 : 1) * list[idx_lc].CompareTo(list[index]) > 0) {
-                T tmp = list[idx_lc];
+            if ((ascent ? -1 : 1) * comparer.Compare(list[idx_lc].key, list[index].key) > 0) {
+                var tmp = list[idx_lc];
                 list[idx_lc] = list[index];
                 list[index] = tmp;
             }
         }
-        else if((ascent ? -1 : 1) * list[idx_lc].CompareTo(list[idx_rc]) > 0) {
-            if((ascent ? -1 : 1) * list[idx_lc].CompareTo(list[index]) > 0) {
-                T tmp = list[idx_lc];
+        else if((ascent ? -1 : 1) * comparer.Compare(list[idx_lc].key, list[idx_rc].key) > 0) {
+            if((ascent ? -1 : 1) * comparer.Compare(list[idx_lc].key, list[index].key) > 0) {
+                var tmp = list[idx_lc];
                 list[idx_lc] = list[index];
                 list[index] = tmp;
-                FilterDown(list, idx_lc, ascent);
+                FilterDown(list, idx_lc, comparer, ascent);
             }
         }
-        else if((ascent ? -1 : 1) * list[idx_rc].CompareTo(list[index]) > 0) {
-            T tmp = list[idx_rc];
+        else if((ascent ? -1 : 1) * comparer.Compare(list[idx_rc].key, list[index].key) > 0) {
+            var tmp = list[idx_rc];
             list[idx_rc] = list[index];
             list[index] = tmp;
-            FilterDown(list, idx_rc, ascent);
+            FilterDown(list, idx_rc, comparer, ascent);
         }
     }
 
-    static void FilterUp(List<T> list, int index, bool ascent = false) {
+    static void FilterUp(List<PriorityQueueEntity<TKey, TValue>> list, int index, IComparer<TKey> comparer, bool ascent = false) {
         int idx_p = (index + 1) / 2 - 1;
-        if(idx_p >= 0 && (ascent ? -1 : 1) * list[index].CompareTo(list[idx_p]) > 0) {
-            T tmp = list[idx_p];
+        if(idx_p >= 0 && (ascent ? -1 : 1) * comparer.Compare(list[index].key, list[idx_p].key) > 0) {
+            var tmp = list[idx_p];
             list[idx_p] = list[index];
             list[index] = tmp;
-            FilterUp(list, idx_p, ascent);
+            FilterUp(list, idx_p, comparer, ascent);
         }
     }
-
-    public void Offer(T val) {
-        list.Add(val);
-        FilterUp(list, list.Count - 1, this.ascent);
+    public void Offer(PriorityQueueEntity<TKey, TValue> entity) {
+        list.Add(entity);
+        FilterUp(list, list.Count - 1, this.comparer, this.ascent);
     }
 
-    public T Peek() {
+    public void Offer(TKey key, TValue val) {
+        this.Offer(new PriorityQueueEntity<TKey, TValue>(key, val));
+    }
+
+    public TValue Peek() {
+        return this.PeekEntity().value;
+    }
+
+    public PriorityQueueEntity<TKey, TValue> PeekEntity() {
         return list[0];
     }
 
-    public T Poll() {
-        T top = list[0];
+    public TValue Poll() {
+        return this.PollEntity().value;
+    }
+
+    public PriorityQueueEntity<TKey, TValue> PollEntity() {
+        var top = list[0];
         list[0] = list[list.Count - 1];
         list.RemoveAt(list.Count - 1);
-        FilterDown(list, 0, this.ascent);
+        FilterDown(list, 0, this.comparer, this.ascent);
         return top;
     }
 
@@ -175,23 +212,23 @@ public class PriorityQueue<T> where T: IComparable<T> {
 }
 
 public class MedianFinder {
-    PriorityQueue<int> pq_d = new PriorityQueue<int>();
-    PriorityQueue<int> pq_a = new PriorityQueue<int>(true);
+    PriorityQueue<int, int> pq_d = new PriorityQueue<int, int>();
+    PriorityQueue<int, int> pq_a = new PriorityQueue<int, int>(true);
     /** initialize your data structure here. */
     public MedianFinder() {
         
     }
     
     public void AddNum(int num) {
-        pq_d.Offer(num);
+        pq_d.Offer(num, num);
         if(pq_a.Count > 0 && pq_d.Peek() > pq_a.Peek()) {
-            pq_a.Offer(pq_d.Poll());
+            pq_a.Offer(pq_d.PollEntity());
         }
         while(pq_d.Count - pq_a.Count > 1) {
-            pq_a.Offer(pq_d.Poll());
+            pq_a.Offer(pq_d.PollEntity());
         }
         while(pq_d.Count - pq_a.Count < 0) {
-            pq_d.Offer(pq_a.Poll());
+            pq_d.Offer(pq_a.PollEntity());
         }
     }
     
